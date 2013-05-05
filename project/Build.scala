48bb0198b34955b8a006fbf8c21189dd5b6f890d
Leonel Sanches da Silva <leonel@designliquido.com.br>
2013-05-04 21:43:03 -0300

    Tasks implementation~

--- project/Build.scala
+++ project/Build.scala
@@ -1,15 +1,18 @@
+import sbt._
+import Keys._
+import play.Project._
 
 object ApplicationBuild extends Build {
 
-  val appName         = "myFirstApp"
+  val appName         = "mySecondApp"
   val appVersion      = "1.0-SNAPSHOT"
 
   val appDependencies = Seq(
     // Add your project dependencies here,
-    jdbc,
-    anorm
+    javaCore,
+    javaJdbc,
+    javaEbean
   )
-
 
   val main = play.Project(appName, appVersion, appDependencies).settings(
     // Add your own project settings here      
